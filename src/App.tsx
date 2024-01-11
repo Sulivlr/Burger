8d2ca68f476aa909dfe3d214367bc2bc5d95a265
Sultan Mukhtarov <suli419956@gmail.com>
2024-01-11 21:56:37 +0600

    Initial Commit. поменял ксс и создал типизацию ингридиента. так-же создал компонент Ингридиенты с пропсами и их типами. Так-же добавил в app массив ингридиентов со стейтом.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,24 @@
 
 import './Burger.css'
+import meatImage from "./assets/meat.png";
+import baconImage from "./assets/Bacon.png";
+import cheeseImage from "./assets/cheese.png";
+import saladImage from "./assets/Salad.png";
+import {useState} from "react";
 
 
 function App() {
 
+
+
+    const INGREDIENTS: Ingredient[] = [
+        {name: 'Meat', price: 80, image: meatImage},
+        {name: 'Bacon', price: 60, image: baconImage},
+        {name: 'Cheese', price: 50, image: cheeseImage},
+        {name: 'Salad', price: 10, image: saladImage}
+    ]
+
+    const [ingredients, setIngredients] = useState<Ingredient>(INGREDIENTS);
 
 
 
--- src/components/Burger/types.d.ts
+++ src/components/Burger/types.d.ts
@@ -1,7 +1,3 @@
-import cheeseImage from '../../assets/cheese.png';
-import baconImage from '../../assets/Bacon.png';
-import saladImage from '../../assets/Salad.png';
-import meatImage from '../../assets/meat.png';
 
 interface Ingredient {
     name: string;
@@ -9,11 +5,4 @@ interface Ingredient {
     image: string;
 }
 
- const INGREDIENTS: Ingredient[] = [
-    {name: 'Meat', Price: 80, image: meatImage},
-    {name: 'Bacon', price: 60, image: baconImage},
-    {name: 'Cheese', price: 50, image: cheeseImage},
-    {name: 'Salad', price: 10, image: saladImage}
-]
 
-export default INGREDIENTS;
